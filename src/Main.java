ad45d8bf2d28bfd321ed96826894396d51761db4
liorpernik <liorpernik@tau.mail.ac.il>
2025-11-03 13:36:58 +0200

    formatted the output

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,85 @@
+
 import java.io.*;
 import java.io.PrintWriter;
 
 import java_cup.runtime.Symbol;
-   
-public class Main
-{
-	static public void main(String argv[])
-	{
-		Lexer l;
-		Symbol s;
-		FileReader fileReader;
-		PrintWriter fileWriter;
-		String inputFileName = argv[0];
-		String outputFileName = argv[1];
-		
-		try
-		{
-			/********************************/
-			/* [1] Initialize a file reader */
-			/********************************/
-			fileReader = new FileReader(inputFileName);
 
-			/********************************/
-			/* [2] Initialize a file writer */
-			/********************************/
-			fileWriter = new PrintWriter(outputFileName);
-			
-			/******************************/
-			/* [3] Initialize a new lexer */
-			/******************************/
-			l = new Lexer(fileReader);
+public class Main {
 
-			/***********************/
-			/* [4] Read next token */
-			/***********************/
-			s = l.next_token();
+    static final String[] TOKEN_NAMES = {
+        "EOF", "PLUS", "MINUS", "TIMES", "DIVIDE", "LPAREN", "RPAREN",
+        "LBRACK", "RBRACK", "LBRACE", "RBRACE", "ASSIGN", "EQ", "LT", "GT",
+        "COMMA", "DOT", "SEMICOLON", "ARRAY", "CLASS", "RETURN", "WHILE",
+        "IF", "ELSE", "NEW", "EXTENDS", "NIL", "TYPE_INT", "TYPE_STRING",
+        "TYPE_VOID", "INT", "STRING", "ID"
+    };
 
-			/********************************/
-			/* [5] Main reading tokens loop */
-			/********************************/
-			while (s.sym != TokenNames.EOF)
-			{
-				/************************/
-				/* [6] Print to console */
-				/************************/
-				System.out.print("[");
-				System.out.print(l.getLine());
-				System.out.print(",");
-				System.out.print(l.getTokenStartPosition());
-				System.out.print("]:");
-				System.out.print(s.value);
-				System.out.print("\n");
-				
-				/*********************/
-				/* [7] Print to file */
-				/*********************/
-				fileWriter.print(l.getLine());
-				fileWriter.print(": ");
-				fileWriter.print(s.value);
-				fileWriter.print("\n");
-				
-				/***********************/
-				/* [8] Read next token */
-				/***********************/
-				s = l.next_token();
-			}
-			
-			/******************************/
-			/* [9] Close lexer input file */
-			/******************************/
-			l.yyclose();
+    static public void main(String argv[]) {
+        Lexer l;
+        Symbol s;
+        FileReader fileReader;
+        PrintWriter fileWriter;
+        String inputFileName = argv[0];
+        String outputFileName = argv[1];
+        try {
+                            /* [2] Initialize a file writer */
 
-			/**************************/
-			/* [10] Close output file */
-			/**************************/
-			fileWriter.close();
-    	}
-			     
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+            fileWriter = new PrintWriter(outputFileName);
+
+            try {
+                /**
+                 * *****************************
+                 */
+                /* [1] Initialize a file reader */
+                /**
+                 * *****************************
+                 */
+                fileReader = new FileReader(inputFileName);
+
+                /* [3] Initialize a new lexer */
+                l = new Lexer(fileReader);
+
+                /* [4] Read next token */
+
+                s = l.next_token();
+
+                /* [5] Main reading tokens loop */
+                while (s.sym != TokenNames.EOF) {
+                    /* Print to file */
+                    fileWriter.print(TOKEN_NAMES[s.sym]);
+                    if (s.sym >= 30) // INT\ID\STRING
+                    {
+                        fileWriter.print("(" + s.value + ")");
+                    }
+                    fileWriter.print(l.getPos());
+
+                    /* Read next token */
+                    s = l.next_token();
+                    if (s.sym != TokenNames.EOF) {
+                        fileWriter.print("\n");
+                    }
+                }
+
+                /* Close lexer input file */
+                l.yyclose();
+
+                /* Close output file */
+                fileWriter.close();
+
+            } catch (Error e) {
+                fileWriter.close();
+                try (PrintWriter errorWriter = new PrintWriter(outputFileName)) { // this truncates the file
+                    errorWriter.print("ERROR");
+                } 
+                catch (Exception x) {
+                    System.err.println("Couldn't open output file1");
+                }
+                // debug
+                // fileWriter.print("error:"+e.getMessage());
+                // fileWriter.close();
+            }
+        } catch (Exception e) {
+            System.err.println("Couldn't open output file");
+        }
+    }
 }
-
-
