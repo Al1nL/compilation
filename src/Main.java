1eb87affb00f16558babdf7f7437c4dffb622fe6
alinl <alinl@mail.tau.ac.il>
2025-11-14 14:14:57 +0200

    initialize 2

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,69 @@
-
 import java.io.*;
 import java.io.PrintWriter;
-
 import java_cup.runtime.Symbol;
+import ast.*;
 
-public class Main {
+public class Main
+{
+	static public void main(String argv[])
+	{
+		Lexer l;
+		Parser p;
+		Symbol s;
+		AstStmtList ast;
+		FileReader fileReader;
+		PrintWriter fileWriter;
+		String inputFileName = argv[0];
+		String outputFileName = argv[1];
+		
+		try
+		{
+			/********************************/
+			/* [1] Initialize a file reader */
+			/********************************/
+			fileReader = new FileReader(inputFileName);
 
-    static final String[] TOKEN_NAMES = {
-        "EOF", "PLUS", "MINUS", "TIMES", "DIVIDE", "LPAREN", "RPAREN",
-        "LBRACK", "RBRACK", "LBRACE", "RBRACE", "ASSIGN", "EQ", "LT", "GT",
-        "COMMA", "DOT", "SEMICOLON", "ARRAY", "CLASS", "RETURN", "WHILE",
-        "IF", "ELSE", "NEW", "EXTENDS", "NIL", "TYPE_INT", "TYPE_STRING",
-        "TYPE_VOID", "INT", "STRING", "ID"
-    };
+			/********************************/
+			/* [2] Initialize a file writer */
+			/********************************/
+			fileWriter = new PrintWriter(outputFileName);
+			
+			/******************************/
+			/* [3] Initialize a new lexer */
+			/******************************/
+			l = new Lexer(fileReader);
+			
+			/*******************************/
+			/* [4] Initialize a new parser */
+			/*******************************/
+			p = new Parser(l);
 
-    static public void main(String argv[]) {
-        Lexer l;
-        Symbol s;
-        FileReader fileReader;
-        PrintWriter fileWriter;
-        String inputFileName = argv[0];
-        String outputFileName = argv[1];
-        try {
-            /* [2] Initialize a file writer */
-            File outputFile = new File(outputFileName);
-            outputFile.getParentFile().mkdirs();
-            fileWriter = new PrintWriter(outputFileName);
-
-            try {
-                /* [1] Initialize a file reader */
-
-                fileReader = new FileReader(inputFileName);
-
-                /* [3] Initialize a new lexer */
-                l = new Lexer(fileReader);
-
-                /* [4] Read next token */
-                s = l.next_token();
-
-                /* [5] Main reading tokens loop */
-                while (s.sym != TokenNames.EOF) {
-                    /* Print to file */
-                    fileWriter.print(TOKEN_NAMES[s.sym]);
-                    if (s.sym >= 30) // INT\ID\STRING
-                    {
-                        fileWriter.print("(" + s.value + ")");
-                    }
-                    fileWriter.print(l.getPos());
-
-                    /* Read next token */
-                    s = l.next_token();
-                    if (s.sym != TokenNames.EOF) {
-                        fileWriter.print("\n");
-                    }
-                }
-
-                /* Close lexer input file */
-                l.yyclose();
-
-                /* Close output file */
-                fileWriter.close();
-
-            } catch (Error e) {
-                fileWriter.close();
-                try (PrintWriter errorWriter = new PrintWriter(outputFileName)) { // truncates the file
-                    errorWriter.print("ERROR");
-                } catch (Exception x) {
-                    System.err.println("Couldn't open output file");
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("Error, see stack trace:");
-            e.printStackTrace();
-        }
-    }
+			/***********************************/
+			/* [5] 3 ... 2 ... 1 ... Parse !!! */
+			/***********************************/
+			ast = (AstStmtList) p.parse().value;
+			
+			/*************************/
+			/* [6] Print the AST ... */
+			/*************************/
+			ast.printMe();
+			
+			/*************************/
+			/* [7] Close output file */
+			/*************************/
+			fileWriter.close();
+			
+			/*************************************/
+			/* [8] Finalize AST GRAPHIZ DOT file */
+			/*************************************/
+			AstGraphviz.getInstance().finalizeFile();
+    	}
+			     
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
+
+
