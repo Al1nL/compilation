fe5b8e419a3b98f382b37be22aad24669a44fa84
liorpernik <liorpernik@tau.mail.ac.il>
2025-11-24 15:04:42 +0200

    print to outputfile

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,102 @@
+
 import java.io.*;
 import java.io.PrintWriter;
 import java_cup.runtime.Symbol;
 import ast.*;
 
-public class Main
-{
-	static public void main(String argv[])
-	{
-		Lexer l;
-		Parser p;
-		Symbol s;
-		AstNode ast;
-		FileReader fileReader;
-		PrintWriter fileWriter;
-		String inputFileName = argv[0];
-		String outputFileName = argv[1];
-		
-		try
-		{
-			/********************************/
-			/* [1] Initialize a file reader */
-			/********************************/
-			fileReader = new FileReader(inputFileName);
+public class Main {
 
-			/********************************/
-			/* [2] Initialize a file writer */
-			/********************************/
-			fileWriter = new PrintWriter(outputFileName);
-			
-			/******************************/
-			/* [3] Initialize a new lexer */
-			/******************************/
-			l = new Lexer(fileReader);
-			
-			/*******************************/
-			/* [4] Initialize a new parser */
-			/*******************************/
-			p = new Parser(l);
+    static public void main(String argv[]) {
+        Lexer l;
+        Parser p;
+        Symbol s;
+        AstNode ast;
+        FileReader fileReader;
+        PrintWriter fileWriter;
+        String inputFileName = argv[0];
+        String outputFileName = argv[1];
 
-			/***********************************/
-			/* [5] 3 ... 2 ... 1 ... Parse !!! */
-			/***********************************/
-			ast = (AstNode) p.parse().value;
-			
-			/*************************/
-			/* [6] Print the AST ... */
-			/*************************/
-			ast.printMe();
-			
-			/*************************/
-			/* [7] Close output file */
-			/*************************/
-			fileWriter.close();
-			
-			/*************************************/
-			/* [8] Finalize AST GRAPHIZ DOT file */
-			/*************************************/
-			AstGraphviz.getInstance().finalizeFile();
-    	}
-			     
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+        try {
+            /**
+             * *****************************
+             */
+            /* [1] Initialize a file reader */
+            /**
+             * *****************************
+             */
+            fileReader = new FileReader(inputFileName);
+
+            /**
+             * *****************************
+             */
+            /* [2] Initialize a file writer */
+            /**
+             * *****************************
+             */
+            fileWriter = new PrintWriter(outputFileName);
+
+            /**
+             * ***************************
+             */
+            /* [3] Initialize a new lexer */
+            /**
+             * ***************************
+             */
+            l = new Lexer(fileReader);
+
+            /**
+             * ****************************
+             */
+            /* [4] Initialize a new parser */
+            /**
+             * ****************************
+             */
+            p = new Parser(l);
+
+            /**
+             * ********************************
+             */
+            /* [5] 3 ... 2 ... 1 ... Parse !!! */
+            /**
+             * ********************************
+             */
+            try {
+                ast = (AstNode) p.parse().value;
+
+                /**
+                 * **********************
+                 */
+                /* [6] Print the AST ... */
+                /**
+                 * **********************
+                 */
+                ast.printMe();
+                fileWriter.print("OK");
+
+                /**
+                 * **********************************
+                 */
+                /* [8] Finalize AST GRAPHIZ DOT file */
+                /**
+                 * **********************************
+                 */
+                AstGraphviz.getInstance().finalizeFile();
+            } catch (Error le) {
+                // lexical error
+                fileWriter.print("ERROR");
+            } catch (Exception e) {
+
+                // syntax error with location
+                Symbol sym = l.next_token();
+                if (sym != null && sym.left >= 0) {
+                    fileWriter.print("ERROR(" + sym.left + ")");
+                } else {
+                    fileWriter.print("ERROR");
+                }
+            }
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-
-
