217ee35ed06362439549300e177042d91c95bf1f
liorpernik <liorpernik@tau.mail.ac.il>
2025-11-24 16:03:42 +0200

    dec list fix print

--- src/ast/AstDecList.java
+++ src/ast/AstDecList.java
@@ -1,29 +1,29 @@
-package ast;
+    package ast;
 
-public class AstDecList extends AstNode {
-    public AstDec head;
-    public AstDecList  tail;
+    public class AstDecList extends AstNode {
+        public AstDec head;
+        public AstDecList  tail;
 
-    public AstDecList(AstDec head, AstDecList tail) {
-        serialNumber = AstNodeSerialNumber.getFresh();
+        public AstDecList(AstDec head, AstDecList tail) {
+            serialNumber = AstNodeSerialNumber.getFresh();
 
-        if (tail != null)
-            System.out.print("===== cFieldList -> cField cFieldList\n");
-        else
-            System.out.print("===== cFieldList -> cField\n");
+            if (tail != null)
+                System.out.print("===== cFieldList -> cField cFieldList\n");
+            else
+                System.out.print("===== cFieldList -> cField\n");
 
-        this.head = head;
-        this.tail = tail;
+            this.head = head;
+            this.tail = tail;
+        }
+        public void printMe() {
+        if (head != null) head.printMe();
+        if (tail != null) tail.printMe();
+
+        
+        AstGraphviz.getInstance().logNode(serialNumber, "DEC LIST");
+
+        if (head != null) AstGraphviz.getInstance().logEdge(serialNumber, head.serialNumber);
+        
+        if (tail != null) AstGraphviz.getInstance().logEdge(serialNumber, tail.serialNumber);
     }
-    public void printMe() {
-    if (head != null) head.printMe();
-    if (tail != null) tail.printMe();
-
-    
-    AstGraphviz.getInstance().logNode(serialNumber, "DEC LIST");
-
-    if (head != null) AstGraphviz.getInstance().logEdge(serialNumber, head.serialNumber);
-    
-    if (tail != null) AstGraphviz.getInstance().logEdge(serialNumber, tail.head.serialNumber);
-}
-}
+    }
