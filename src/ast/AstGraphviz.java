a119b6672d455a4f3d47fc6c9b23b307e00746d9
liorpernik <liorpernik@tau.mail.ac.il>
2025-11-24 17:28:07 +0200

    dir check dot file

--- src/ast/AstGraphviz.java
+++ src/ast/AstGraphviz.java
@@ -33,8 +33,9 @@ public class AstGraphviz
 			/****************************/
 			try
 			{
-				String dirname="./output/";
-				String filename="AST_IN_GRAPHVIZ_DOT_FORMAT.txt";
+                String dirname = "./AST_output/";
+                java.io.File dir = new java.io.File(dirname);
+                String filename="AST_IN_GRAPHVIZ_DOT_FORMAT.txt";
 				instance.fileWriter = new PrintWriter(dirname+filename);
 			}
 			catch (Exception e)
