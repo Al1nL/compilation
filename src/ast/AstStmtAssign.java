0e6a4a6798ec964ae29c7d3af0240d60d4fbb9f7
liorpernik <liorpernik@tau.mail.ac.il>
2025-11-28 19:53:04 +0200

    Assignments

--- src/ast/AstStmtAssign.java
+++ src/ast/AstStmtAssign.java
@@ -1,80 +1,117 @@
 package ast;
+
 import types.*;
 
+public class AstStmtAssign extends AstStmt {
 
-public class AstStmtAssign extends AstStmt
-{
-	/***************/
-	/*  var := exp */
-	/***************/
-	public AstVar var;
-	public AstExp exp;
+    /**
+     * ************
+     */
+    /*  var := exp */
+    /**
+     * ************
+     */
+    public AstVar var;
+    public AstExp exp;
 
-	/*******************/
-	/*  CONSTRUCTOR(S) */
-	/*******************/
-	public AstStmtAssign(AstVar var, AstExp exp)
-	{
-		/******************************/
-		/* SET A UNIQUE SERIAL NUMBER */
-		/******************************/
-		serialNumber = AstNodeSerialNumber.getFresh();
+    /**
+     * ****************
+     */
+    /*  CONSTRUCTOR(S) */
+    /**
+     * ****************
+     */
+    public AstStmtAssign(AstVar var, AstExp exp) {
+        /**
+         * ***************************
+         */
+        /* SET A UNIQUE SERIAL NUMBER */
+        /**
+         * ***************************
+         */
+        serialNumber = AstNodeSerialNumber.getFresh();
 
-		/***************************************/
-		/* PRINT CORRESPONDING DERIVATION RULE */
-		/***************************************/
-		System.out.print("====================== stmt -> var ASSIGN exp SEMICOLON\n");
+        /**
+         * ************************************
+         */
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /**
+         * ************************************
+         */
+        System.out.print("====================== stmt -> var ASSIGN exp SEMICOLON\n");
 
-		/*******************************/
-		/* COPY INPUT DATA MEMBERS ... */
-		/*******************************/
-		this.var = var;
-		this.exp = exp;
-	}
+        /**
+         * ****************************
+         */
+        /* COPY INPUT DATA MEMBERS ... */
+        /**
+         * ****************************
+         */
+        this.var = var;
+        this.exp = exp;
+    }
 
-	/*********************************************************/
-	/* The printing message for an assign statement AST node */
-	/*********************************************************/
-	public void printMe()
-	{
-		/********************************************/
-		/* AST NODE TYPE = AST ASSIGNMENT STATEMENT */
-		/********************************************/
-		System.out.print("AST NODE ASSIGN STMT\n");
+    /**
+     * ******************************************************
+     */
+    /* The printing message for an assign statement AST node */
+    /**
+     * ******************************************************
+     */
+    public void printMe() {
+        /**
+         * *****************************************
+         */
+        /* AST NODE TYPE = AST ASSIGNMENT STATEMENT */
+        /**
+         * *****************************************
+         */
+        System.out.print("AST NODE ASSIGN STMT\n");
 
-		/***********************************/
-		/* RECURSIVELY PRINT VAR + EXP ... */
-		/***********************************/
-		if (var != null) var.printMe();
-		if (exp != null) exp.printMe();
+        /**
+         * ********************************
+         */
+        /* RECURSIVELY PRINT VAR + EXP ... */
+        /**
+         * ********************************
+         */
+        if (var != null) {
+            var.printMe();
+        }
+        if (exp != null) {
+            exp.printMe();
+        }
 
-		/***************************************/
-		/* PRINT Node to AST GRAPHVIZ DOT file */
-		/***************************************/
-		AstGraphviz.getInstance().logNode(
-				serialNumber,
-			"ASSIGN\nleft := right\n");
-		
-		/****************************************/
-		/* PRINT Edges to AST GRAPHVIZ DOT file */
-		/****************************************/
-		AstGraphviz.getInstance().logEdge(serialNumber,var.serialNumber);
-		AstGraphviz.getInstance().logEdge(serialNumber,exp.serialNumber);
-	}
+        /**
+         * ************************************
+         */
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /**
+         * ************************************
+         */
+        AstGraphviz.getInstance().logNode(
+                serialNumber,
+                "ASSIGN\nleft := right\n");
 
-	
-	public Type semantMe()
-	{
-		Type t1 = null;
-		Type t2 = null;
-		
-		if (var != null) t1 = var.semantMe();
-		if (exp != null) t2 = exp.semantMe();
-		
-		if (t1 != t2)
-		{
-			System.out.format(">> ERROR [%d:%d] type mismatch for var := exp\n",6,6);				
-		}
-		return null;
-	}
+        /**
+         * *************************************
+         */
+        /* PRINT Edges to AST GRAPHVIZ DOT file */
+        /**
+         * *************************************
+         */
+        AstGraphviz.getInstance().logEdge(serialNumber, var.serialNumber);
+        AstGraphviz.getInstance().logEdge(serialNumber, exp.serialNumber);
+    }
+
+    public void semantMe(Type expectedReturnType) {
+        Type varType = var.semantMe();
+        Type expType = exp.semantMe();
+
+        if (!expType.canAssignTo(varType)) {
+            System.err.println("ERROR: Cannot assign " + expType.name + " to " + varType.name);
+			report();
+        }
+    }
+
 }
--- src/types/Type.java
+++ src/types/Type.java
@@ -1,76 +1,100 @@
 package types;
 
-public abstract class Type
-{
-	/******************************/
-	/*  Every type has a name ... */
-	/******************************/
-	public String name;
+public abstract class Type {
 
-	/*************/
-	/* isClass() */
-	/*************/
-	public boolean isClass(){ return false;}
+    /**
+     * ***************************
+     */
+    /*  Every type has a name ... */
+    /**
+     * ***************************
+     */
+    public String name;
 
-	/*************/
-	/* isArray() */
-	/*************/
-	public boolean isArray(){ return false;}
-
-    public boolean isSameType(Type other)
-    {
-        if (other == null) return false;
-        
-        // Handle nil - nil can be assigned to any class or array type
-        if (other instanceof TypeNil)
-        {
-            return (this instanceof TypeClass || this instanceof TypeArray);
-        }
-        if (this instanceof TypeNil)
-        {
-            return (other instanceof TypeClass || other instanceof TypeArray);
-        }
-        
-        // Same primitive types
-        if (this instanceof TypeInt && other instanceof TypeInt) return true;
-        if (this instanceof TypeString && other instanceof TypeString) return true;
-        if (this instanceof TypeVoid && other instanceof TypeVoid) return true;
-        
-        // Same named types (classes/arrays)
-        if (this.name != null && other.name != null)
-        {
-            return this.name.equals(other.name);
-        }
-        
+    /**
+     * **********
+     */
+    /* isClass() */
+    /**
+     * **********
+     */
+    public boolean isClass() {
         return false;
     }
 
-    public boolean canAssignTo(Type target)
-    {
-        // Same type can always be assigned
-        if (isSameType(target)) return true;
-        
-        // Nil can be assigned to any class or array
-        if (this instanceof TypeNil && (target instanceof TypeClass || target instanceof TypeArray))
-        {
+    /**
+     * **********
+     */
+    /* isArray() */
+    /**
+     * **********
+     */
+    public boolean isArray() {
+        return false;
+    }
+
+    public boolean isSameType(Type other) {
+        if (other == null) {
+            return false;
+        }
+
+        // Handle nil - nil can be assigned to any class or array type
+        if (other instanceof TypeNil) {
+            return (this instanceof TypeClass || this instanceof TypeArray);
+        }
+        if (this instanceof TypeNil) {
+            return (other instanceof TypeClass || other instanceof TypeArray);
+        }
+
+        // Same primitive types
+        if (this instanceof TypeInt && other instanceof TypeInt) {
             return true;
         }
-        
+        if (this instanceof TypeString && other instanceof TypeString) {
+            return true;
+        }
+        if (this instanceof TypeVoid && other instanceof TypeVoid) {
+            return true;
+        }
+
+        // Same named types (classes/arrays)
+        if (this.name != null && other.name != null) {
+            return this.name.equals(other.name);
+        }
+
+        return false;
+    }
+
+    public boolean canAssignTo(Type target) {
+        // Same type can always be assigned
+        if (isSameType(target)) {
+            return true;
+        }
+
+        // Nil can be assigned to any class or array
+        if (this instanceof TypeNil && (target instanceof TypeClass || target instanceof TypeArray)) {
+            return true;
+        }
+
+         if (this instanceof TypeArray && target instanceof TypeArray) {
+            return false;
+        }
+
         // Check class inheritance
-        if (this instanceof TypeClass && target instanceof TypeClass)
-        {
-            TypeClass thisClass = (TypeClass) this;
-            TypeClass targetClass = (TypeClass) target;
-            
+        if (this instanceof TypeClass && target instanceof TypeClass) {
+            TypeClass thisClass = (TypeClass) this; // type of expression e (e.g., Son)
+            TypeClass targetClass = (TypeClass) target; // type of variable x (e.g., Father)
+
             // Check if thisClass is a subclass of targetClass
             TypeClass current = thisClass;
-            while (current != null)
-            {
-                if (current.name.equals(targetClass.name)) return true;
+            while (current != null) {
+                if (current.name.equals(targetClass.name)) {
+                    return true;
+                }
                 current = current.father;
             }
         }
-        
+
         return false;
     }
 }
