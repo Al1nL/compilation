b26d5d4c297aee24d556b27390592a967ab75f3e
liorpernik <liorpernik@tau.mail.ac.il>
2025-12-04 16:24:52 +0200

    debug prints

--- src/ast/AstDecClass.java
+++ src/ast/AstDecClass.java
@@ -5,30 +5,16 @@ import types.*;
 
 public class AstDecClass extends AstDec {
 
-    /**
-     * *************
-     */
-    /* DATA MEMBERS */
-    /**
-     * *************
-     */
     public final String name;         // class name
     public final String parentName;   // null if no EXTENDS
     public final AstDecList fields;   // linked list of cFields
 
-    /**
-     * ***************
-     */
-    /* CONSTRUCTOR(S) */
-    /**
-     * ***************
-     */
     public AstDecClass(String name, String parentName, AstDecList fields) {
         serialNumber = AstNodeSerialNumber.getFresh();
 
         if (parentName != null) {
-            System.out.print("===== classDec -> CLASS ID EXTENDS ID LBRACE cField { cField } RBRACE\n"); 
-        }else {
+            System.out.print("===== classDec -> CLASS ID EXTENDS ID LBRACE cField { cField } RBRACE\n");
+        } else {
             System.out.print("===== classDec -> CLASS ID LBRACE cField { cField } RBRACE\n");
         }
 
@@ -39,67 +25,61 @@ public class AstDecClass extends AstDec {
 
     @Override
     public void printMe() {
-        /**
-         * **********************************
-         */
-        /* RECURSIVELY PRINT HEAD + TAIL ... */
-        /**
-         * **********************************
-         */
         System.out.format("CLASS DEC = %s\n", this.name);
+
         if (fields != null) {
             fields.printMe();
         }
 
-        /**
-         * ************************************
-         */
-        /* PRINT Node to AST GRAPHVIZ DOT file */
-        /**
-         * ************************************
-         */
         AstGraphviz.getInstance().logNode(
-                serialNumber,
-                String.format("CLASS\n%s", this.name));
+            serialNumber,
+            String.format("CLASS\n%s", this.name));
 
-        /**
-         * *************************************
-         */
-        /* PRINT Edges to AST GRAPHVIZ DOT file */
-        /**
-         * *************************************
-         */
         AstGraphviz.getInstance().logEdge(serialNumber, fields.serialNumber);
     }
 
     public Type semantMe() {
+
+        System.err.println("\n====== CLASS " + this.name + " START ======");
+        System.err.println("topIndex BEFORE enter: " + SymbolTable.getInstance().topIndex);
+
+        // Check duplicate class
         if (SymbolTable.getInstance().find(this.name) != null) {
-            report(); // class already declared
+            report();
         }
 
+        // Check parent
         TypeClass parentTypeClass = null;
-        //todo: check this after
-
         if (parentName != null) {
             Type parentType = SymbolTable.getInstance().find(parentName);
             if (parentType == null || !(parentType instanceof TypeClass)) {
-                report(); // superclass does not exist or is not a class
+                report();
             }
             parentTypeClass = (TypeClass) parentType;
         }
 
+        // Create empty class type first
         TypeClass t = new TypeClass(parentTypeClass, this.name, null);
+
+        // ENTER class name
         SymbolTable.getInstance().enter(this.name, t);
+        System.err.println("topIndex AFTER enter class name: " + SymbolTable.getInstance().topIndex);
 
+        // BEGIN SCOPE
         SymbolTable.getInstance().beginScope();
+        System.err.println("topIndex AFTER beginScope: " + SymbolTable.getInstance().topIndex);
 
-        /*  Semant Data Members */
+        // PROCESS FIELDS
+        System.err.println(">>> Processing fields for " + this.name);
         TypeList fieldTypes = fields.semantMe();
+        System.err.println("topIndex AFTER processing fields: " + SymbolTable.getInstance().topIndex);
 
-        // Update the TypeClass with the actual field types
+        // Store fields in class type
         t.dataMembers = fieldTypes;
 
-        /*  DON'T End Scope - class scope stays open!     */
+        System.err.println("====== CLASS " + this.name + " END ======\n");
+
+        // DO NOT endScope()
         return t;
     }
 
