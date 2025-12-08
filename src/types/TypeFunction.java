4ce3b3b44552945b0a02ef3d05569e10c96f7bb3
liorpernik <liorpernik@tau.mail.ac.il>
2025-12-08 14:20:38 +0200

    edge case null args fixed

--- src/types/TypeFunction.java
+++ src/types/TypeFunction.java
@@ -1,52 +1,70 @@
 package types;
 
-public class TypeFunction extends Type
-{
-	/***********************************/
-	/* The return type of the function */
-	/***********************************/
-	public Type returnType;
+public class TypeFunction extends Type {
 
-	/*************************/
-	/* types of input params */
-	/*************************/
-	public TypeList params;
-	
-	/****************/
-	/* CTROR(S) ... */
-	/****************/
-	public TypeFunction(Type returnType, String name, TypeList params)
-	{
-		this.name = name;
-		this.returnType = returnType;
-		this.params = params;
-	}
+    /**
+     * ********************************
+     */
+    /* The return type of the function */
+    /**
+     * ********************************
+     */
+    public Type returnType;
 
-	public boolean compareFunctions(TypeFunction other){
-		// return type must match exactly
-		if (!this.returnType.equals(other.returnType)){
+    /**
+     * **********************
+     */
+    /* types of input params */
+    /**
+     * **********************
+     */
+    public TypeList params;
+
+    /**
+     * *************
+     */
+    /* CTROR(S) ... */
+    /**
+     * *************
+     */
+    public TypeFunction(Type returnType, String name, TypeList params) {
+        this.name = name;
+        this.returnType = returnType;
+        this.params = params;
+    }
+
+    public boolean compareFunctions(TypeFunction other) {
+        // return type must match exactly
+        if (!this.returnType.equals(other.returnType)) {
+            return false;
+        }
+		boolean a1=this.params == null;
+		boolean a2=other.params == null;
+
+        // params must match exactly
+		if((a1 &&!a2)||(!a1&&a2)){
 			return false;
-		}
-			
-
-		// params must match exactly
-		if (this.params.len != other.params.len)
+        }
+		if(a1&&a2)
 		{
-			return false;
+			return true;
 		}
+        if (this.params.len != other.params.len) {
+            return false;
+        }
 
-		TypeList p1 = this.params;
-		TypeList p2 = other.params;
-		while (p1 != null && p2 != null) {
+        TypeList p1 = this.params;
+        TypeList p2 = other.params;
+        while (p1 != null && p2 != null) {
 
-			if (!p1.head.equals(p2.head)) {
-				return false;
-			}
+            if (!p1.head.equals(p2.head)) {
+                return false;
+            }
 
-			p1 = p1.tail;
-			p2 = p2.tail;
-		}
-			
-		return true;
-	}
+            p1 = p1.tail;
+            p2 = p2.tail;
+        }
+
+        return true;
+    }
 }
