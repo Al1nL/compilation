6d9857050da7f396751cedcda0402a003573315e
liorpernik <liorpernik@tau.mail.ac.il>
2025-12-24 19:15:41 +0200

    DEBUG FILE

---
+++ src/analysis/Dbg.java
@@ -0,0 +1,28 @@
+package analysis;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class Dbg {
+    public static boolean ON = true;
+    public static PrintWriter out;
+
+    static {
+        try {
+            out = new PrintWriter(new FileWriter("output/dfa_debug.txt"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void p(String s) {
+        if (!ON) return;
+        out.println(s);
+        out.flush();
+    }
+
+    public static void close() {
+        out.flush();
+        out.close();
+    }
+}
