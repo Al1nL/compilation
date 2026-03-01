115c56769e267b6317baf79a410aae1e9530a18a
liorpernik <liorpernik@tau.mail.ac.il>
2026-03-01 21:43:22 +0200

    add offset to addresses

--- src/ir/IrCommandAddOffset.java
+++ src/ir/IrCommandAddOffset.java
@@ -6,13 +6,13 @@ import java.util.*;
 import mips.MipsGenerator;
 import temp.*;
 
-public class IrCommandAdd extends IrCommand {
+public class IrCommandAddOffset extends IrCommand {
 
     public Temp dst;   // destination Temp
     public Temp t1;    // first operand
     public Temp t2;    // second operand
 
-    public IrCommandAdd(Temp dst, Temp t1, Temp t2) {
+    public IrCommandAddOffset(Temp dst, Temp t1, Temp t2) {
         this.dst = dst;
         this.t1 = t1;
         this.t2 = t2;
